76cb122f6d2c5cdecdc591e44428888f8985c7d4
Brian Deane <bdeanemachine@gmail.com>
2015-01-06 21:08:50 +0000

    Small changes to reset display

--- app/models/reset.rb
+++ app/models/reset.rb
@@ -2,4 +2,18 @@ class Reset < ActiveRecord::Base
   belongs_to :activity
 
   validates :activity_id, :datetime, presence: true
+
+  # eg. Sunday, Monday...
+  def day_name
+    datetime.strftime("%A")
+  end
+
+  # eg. January, February...
+  def month_name
+    datetime.strftime("%B")
+  end
+
+  def datetime_display
+    datetime.strftime("%A, %e %B, %Y - %H:%M")
+  end
 end
