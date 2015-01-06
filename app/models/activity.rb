2a7cd7d8c7a11a64a12e1384d36e3b97673a608d
Brian Deane <bdeanemachine@gmail.com>
2015-01-06 20:12:45 +0000

    Show last reset time

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,8 @@ class Activity < ActiveRecord::Base
   has_many :resets
 
   validates :name, presence: true, uniqueness: true
+
+  def last_reset
+    Reset.where(activity_id: id).order(datetime: :desc).limit(1).first
+  end
 end
